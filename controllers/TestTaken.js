cb89c932e89a605bd1bbfd139a5fb627698732a8
jamfurell <hong.j92@gmail.com>
2021-04-09 21:31:13 -0700

    add draft update/delete for testtaken

--- controllers/TestTaken.js
+++ controllers/TestTaken.js
@@ -1,37 +1,70 @@
 const db = require("../models");
 const index = (req, res) => {
-        db.TestTaken.find({}, (err, foundTestTakens) => {
-            if (err) {
-            console.log("Error in TestTakens#index:", err);
-            return res.status(500).json({ message: "Error. Please try again." });
-            }
-            if (!foundTestTakens.length) {
-            return res.status(200).json({ message: "No TestTakens Found in database." });
-            }
-            res.status(200).json({ TestTakens: foundTestTakens });
+    db.TestTaken.find({}, (err, foundTestTakens) => {
+        if (err) {
+        console.log("Error in TestTakens#index:", err);
+        return res.status(500).json({ message: "Error. Please try again." });
+        }
+        if (!foundTestTakens.length) {
+        return res.status(200).json({ message: "No TestTakens Found in database." });
+        }
+        res.status(200).json({ TestTakens: foundTestTakens });
+    });
+};
+
+const show = (req, res) => {
+    db.TestTaken.findById(req.params.id, (err, foundTestTaken) => {
+        if (err) console.log("Error in TestTakens#show:", err);
+        if (!foundTestTaken) {
+        return res
+            .status(200)
+            .json({ message: "TestTaken with provided ID is not found." });
+        }
+        res.status(200).json({ TestTaken: foundTestTaken });
+    });
+};
+
+const create = (req, res) => {
+        console.log(req.body, "<====req.body")
+    db.TestTaken.create(req.body, (err, savedTestTaken) => {
+        if (err) console.log("Error in TestTakens#create:", err);
+        res.status(201).json({ TestTaken: savedTestTaken });
+    });
+};
+     
+const update = (req, res) => {
+    db.TestTaken.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedTestTaken) => {
+        if (err) console.log("Error in TestTaken#update:", err);
+        if (!updatedTestTaken) {
+            return res.status(200).json({
+            message: "TestTaken with provided ID could not be found for update.",
+            });
+        }
+        res.status(200).json({ TestTaken: updatedTestTaken });
+        }
+    );
+};
+const destroy = (req, res) => {
+    db.TestTaken.findByIdAndDelete(req.params.id, (err, deletedTestTaken) => {
+        if (err) console.log("Error in TestTaken#destroy:", err);
+        if (!deletedTestTaken) {
+        return res.status(200).json({
+            message: "TestTaken with provided ID could not be found for delete.",
         });
-        };
-        const show = (req, res) => {
-        db.TestTaken.findById(req.params.id, (err, foundTestTaken) => {
-            if (err) console.log("Error in TestTakens#show:", err);
-            if (!foundTestTaken) {
-            return res
-                .status(200)
-                .json({ message: "TestTaken with provided ID is not found." });
-            }
-            res.status(200).json({ TestTaken: foundTestTaken });
-        });
-        };
-        const create = (req, res) => {
-            console.log(req.body, "<====req.body")
-        db.TestTaken.create(req.body, (err, savedTestTaken) => {
-            if (err) console.log("Error in TestTakens#create:", err);
-            res.status(201).json({ TestTaken: savedTestTaken });
-        });
-        };
-        
+        }
+        res.status(200).json({ TestTaken: deletedTestTaken });
+    });
+};
+
+
         module.exports = {
         index,
         show,
         create,
+        update,
+        destroy,
     };
--- routes/TestTaken.js
+++ routes/TestTaken.js
@@ -5,5 +5,7 @@ const ctrl = require('../controllers');
 router.get('/', ctrl.TestTaken.index)
 router.get('/:id', ctrl.TestTaken.show);
 router.post('/', ctrl.TestTaken.create);
+router.put('/:id', ctrl.TestTaken.update);
+router.delete('/:id', ctrl.TestTaken.destroy);
 
 module.exports = router
